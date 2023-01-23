3a85f178afbce4534a1f36637ae6a71c6361ba7b
avispro20 <solomonok07@gmail.com>
2023-01-23 05:48:56 -0800

    Update task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.175.133.126', '54.152.236.229']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
