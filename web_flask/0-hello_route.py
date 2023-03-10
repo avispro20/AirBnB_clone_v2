a6c25cf0f8c11dce51d5567770368c4ac85e64a4
avispro20 <solomonok07@gmail.com>
2023-03-10 04:32:28 +0000

    Update to Airbnb-onepage

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,14 +5,12 @@ Your web application must be listening on 0.0.0.0, port 5000
 
 from flask import Flask
 
-app = Flask("__name__")
+app = Flask("app")
 
-
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello():
     """Return a given string"""
     return ("Hello HBNB!")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
